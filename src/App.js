2ae587f708d805397275022aef99be36cf1292c9
Cristiam Da Silva <cristiam@iamtech.tech>
2021-07-26 07:52:17 +0200

    read from redux on app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { useSelector } from "react-redux";
+
 const App = () => {
+  const counter = useSelector(state => state.counter);
   return (
-    <h1>React app</h1>
+    <>
+      <h1>React and Redux</h1>
+      <h2>Counter: {counter}</h2>
+    </>
   )
 }
 
